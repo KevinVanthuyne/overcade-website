3ec007cfde9828b5a0546cb240de93787d93ffdb
Kevin Vanthuyne <kevin.vanthuyne2@gmail.com>
2024-12-28 14:25:27 +0100

    Setup postcss

--- Gemfile
+++ Gemfile
@@ -9,5 +9,7 @@ gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 group :jekyll_plugins do
   gem "jekyll-sitemap"
   gem "jekyll-seo-tag"
+#   gem "jekyll-postcss"
+  gem "jekyll-postcss-v2"
 end
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_games/*.md',
+    './*.md',
+    './*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
